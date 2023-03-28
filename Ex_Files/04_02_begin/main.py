40d9bc3e4367c2f1be67d2735049fe655cb2d99b
SamF543 <103508237+SamF543@users.noreply.github.com>
2023-03-28 14:44:06 +0000

    hello world web app

--- Ex_Files/04_02_begin/main.py
+++ Ex_Files/04_02_begin/main.py
@@ -1,3 +1,11 @@
+# pip install flask
+
 from flask import Flask
 
-app = Flask(__name__)
+app = Flask(__name__) #flask object
+
+@app.route("/")
+def hello():
+    return "Hello, World!"
+
+app.run(debug=True)
