cb79f3c3779bd629decee85c8608f1287ef25348
SamF543 <103508237+SamF543@users.noreply.github.com>
2023-03-28 10:54:17 +0000

    comments added

--- Ex_Files/03_03_begin/main.py
+++ Ex_Files/03_03_begin/main.py
@@ -15,11 +15,11 @@ einstein_json = json.dumps(EINSTEIN)
 back_to_dict = json.loads(einstein_json)
 print(einstein_json)
 pprint(back_to_dict)
-
+#r is read mode f is file object
 with open("laureates.csv", "r") as f:
     reader = csv.DictReader(f)
     laureates = list(reader)
 
-
+#w is write mode
 with open("laureates.json", "w") as f:
     json.dump(laureates, f, indent=2)
